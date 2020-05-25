1676780535e9383b13b9b4e62ef852f3908bff74
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-05-25 17:46:10 +0300

    feat: add sticky header

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,8 @@ testWebP(function (support) {
         document.querySelector("body").classList.add("webp");
     }
 });
+
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
