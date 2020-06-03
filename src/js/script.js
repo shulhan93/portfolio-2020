f2412bc5f9d879cd193e2c845c253c59abb12895
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-06-03 22:16:36 +0300

    feat: fix adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -21,29 +21,31 @@ window.addEventListener('scroll', function () {
 })
 
 $(document).ready(function () {
-
-    $('.slider').slick({});
-    const arrowP = document.querySelector('.slick-arrow.slick-prev')
-    const arrowN = document.querySelector('.slick-next')
-    const lengthSlider = document.querySelectorAll('.slick-cloned').length - 1
-    const allCount = document.querySelector('.slider__count-all')
-    const currentSlider = document.querySelector('.slider__count-current')
-    allCount.textContent = lengthSlider
-    currentSlider.textContent = 1
-    console.log(arrowN)
-    arrowN.addEventListener('click', () => {
-        if (+currentSlider.textContent >= lengthSlider) {
-            return
-        }
-        currentSlider.textContent = +currentSlider.textContent + 1
+    $('.main-nav__burger').click(function (e) {
+        $('.main-nav, .main-nav__burger').toggleClass('active')
     })
+    $('.slider').slick({});
+    /*     const arrowP = document.querySelector('.slick-arrow.slick-prev')
+        const arrowN = document.querySelector('.slick-next')
+        const lengthSlider = document.querySelectorAll('.slick-cloned').length - 1
+        const allCount = document.querySelector('.slider__count-all')
+        const currentSlider = document.querySelector('.slider__count-current')
+        allCount.textContent = lengthSlider
+        currentSlider.textContent = 1
+        console.log(arrowN)
+        arrowN.addEventListener('click', () => {
+            if (+currentSlider.textContent >= lengthSlider) {
+                return
+            }
+            currentSlider.textContent = +currentSlider.textContent + 1
+        })
 
-    arrowP.addEventListener('click', () => {
-        if (+currentSlider.textContent == 1) {
-            return
-        }
-        currentSlider.textContent = +currentSlider.textContent - 1
-    });
+        arrowP.addEventListener('click', () => {
+            if (+currentSlider.textContent == 1) {
+                return
+            }
+            currentSlider.textContent = +currentSlider.textContent - 1
+        }); */
     new WOW({
         offset: 20
     }).init();
