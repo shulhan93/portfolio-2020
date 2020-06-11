2f398814c1e0bb4cf80592d3963cc212a2a95a21
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-06-11 16:44:45 +0300

    feat: ready site

--- src/js/script.js
+++ src/js/script.js
@@ -16,83 +16,81 @@ testWebP(function (support) {
 let error = 0;
 
 $(document).ready(function () {
-
     // sticky header
-    window.addEventListener('scroll', function () {
-        const header = document.querySelector('header')
-        header.classList.toggle('sticky', window.scrollY > 0)
-    })
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector("header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
 
     // count file
-    let fields = document.querySelectorAll('.field__file');
+    let fields = document.querySelectorAll(".field__file");
     Array.prototype.forEach.call(fields, function (input) {
         let label = input.nextElementSibling,
-            labelVal = label.querySelector('.field__file-fake').innerText;
+            labelVal = label.querySelector(".field__file-fake").innerText;
 
-        input.addEventListener('change', function (e) {
-            let countFiles = '';
+        input.addEventListener("change", function (e) {
+            let countFiles = "";
             if (this.files && this.files.length >= 1)
                 countFiles = this.files.length;
 
             if (countFiles)
-                label.querySelector('.field__file-fake').innerText = 'Выбрано файлов: ' + countFiles;
-            else
-                label.querySelector('.field__file-fake').innerText = labelVal;
+                label.querySelector(".field__file-fake").innerText =
+                    "Выбрано файлов: " + countFiles;
+            else label.querySelector(".field__file-fake").innerText = labelVal;
         });
     });
 
-    // validator 
-    const a = $('#form').validate({
+    // validator
+    const a = $("#form").validate({
         rules: {
             name: {
                 required: true,
-            }
+            },
         },
         messages: {
             name: {
                 required: "Поле 'ФИО' обязательно к заполнению",
-                minlength: "Введите не менее 2-х символов в поле 'Имя'"
+                minlength: "Введите не менее 2-х символов в поле 'Имя'",
             },
             email: {
                 required: "Поле 'Email' обязательно к заполнению",
-                email: "Необходим формат адреса email"
+                email: "Необходим формат адреса email",
             },
-            text: "Поле 'Сообщение' обязательно к заполнению"
-        }
-    })
+            text: "Поле 'Сообщение' обязательно к заполнению",
+        },
+    });
 
-
-    // send form 
-    const form = document.getElementById('form')
-    form.addEventListener('submit', function (event) {
-
+    // send form
+    const form = document.getElementById("form");
+    form.addEventListener("submit", function (event) {
         if (!a.valid()) {
-            return false
+            return false;
         }
+        const preloader = document.querySelector(".preloader");
+        preloader.classList.remove("loaded");
         console.log("Отправка запроса");
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        event.preventDefault
+            ? event.preventDefault()
+            : (event.returnValue = false);
 
         var req = new XMLHttpRequest();
-        req.open('POST', 'send.php', true);
+        req.open("POST", "send.php", true);
         req.onload = function () {
             if (req.status >= 200 && req.status < 400) {
                 const json = JSON.parse(this.response); // Ебанный internet explorer 11
-                console.log(json);
 
-                // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
                 if (json.result == "success") {
-                    showSucces()
+                    preloader.classList.add("loaded_hiding", "loaded");
+                    showSucces();
                     // alert("Сообщение отправлено");
-                    document.querySelector('.field__file-fake').textContent = 'Файл не выбран'
-                    form.reset()
+                    document.querySelector(".field__file-fake").textContent =
+                        "Файл не выбран";
+                    form.reset();
                 } else {
-                    // Если произошла ошибка
                     alert("Ошибка. Сообщение не отправлено");
                 }
-                // Если не удалось связаться с php файлом
             } else {
-
-                // alert("Ошибка сервера. Номер: " + req.status);
+                showError();
             }
         };
 
@@ -101,40 +99,89 @@ $(document).ready(function () {
             alert("Ошибка отправки запроса");
         };
         req.send(new FormData(event.target));
-    })
-
+    });
 
     // show modal form
     function showSucces() {
-        const inputName = document.querySelector('input[name="name"]')
-        const modal = document.querySelector('.form__overlay')
-        const close = document.querySelector('.form__modal-close')
-        modal.querySelector('.form__modal-name').textContent = inputName.value
-        modal.classList.add('active')
-        close.addEventListener('click', function () {
-            modal.classList.remove('active')
-        })
+        const inputName = document.querySelector('input[name="name"]');
+        const modal = document.querySelector(".form__overlay");
+        const close = document.querySelector(".form__modal-close");
+        modal.querySelector(".form__modal-name").textContent = inputName.value;
+        modal.classList.add("active");
+        close.addEventListener("click", function () {
+            modal.classList.remove("active");
+        });
+    }
 
+    function showError() {
+        const inputName = document.querySelector('input[name="name"]');
+        const modal = document.querySelector(".form__overlay");
+        const close = document.querySelector(".form__modal-close");
+        const text = document.querySelector(".form__modal-text");
+        text.textContent = "Сообщение не отправлено!!!";
+        modal.querySelector(".form__modal-name").textContent = inputName.value;
+        modal.classList.add("active");
+        close.addEventListener("click", function () {
+            modal.classList.remove("active");
+        });
     }
 
     // burger
-    $('.main-nav__burger').click(function (e) {
-        $('.main-nav, .main-nav__burger').toggleClass('active')
-        $('body').toggleClass('lock')
-    })
+    $(".main-nav__burger").click(function (e) {
+        $(".main-nav, .main-nav__burger").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+
+    $(".main-nav__link").click(function (e) {
+        if ($(".main-nav__burger").hasClass("active")) {
+            $(".main-nav, .main-nav__burger").removeClass("active");
+            $("body").removeClass("lock");
+        }
+    });
 
     // slider
-    $('.slider').slick({
-        responsive: [{
-            breakpoint: 576,
-            settings: {
-                dots: true,
-            }
-        }]
+    $(".slider").slick({
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    dots: true,
+                },
+            },
+        ],
     });
 
     // WOW plugin
     new WOW({
         /*    offset: 10 */
     }).init();
+
+    // scroll
+    $('a[href^="#"]').on("click", function (event) {
+        event.preventDefault();
+
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+
+        $("html, body").animate({ scrollTop: dn }, 700);
+    });
+
+    $(window)
+        .scroll(function () {
+            var windscroll = $(window).scrollTop();
+            if (windscroll >= 100) {
+                $("nav").addClass("fixed");
+                $("section").each(function (i) {
+                    if ($(this).position().top <= windscroll + 50) {
+                        $("nav a.active").removeClass("active");
+                        $("nav a").eq(i).addClass("active");
+                    }
+                });
+            } else {
+                $("nav").removeClass("fixed");
+                $("nav a.active").removeClass("active");
+                $("nav a:first").addClass("active");
+            }
+        })
+        .scroll();
 });
